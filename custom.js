fb190527b6bd678eade89542e9f21fa6e8e5f531
Abraham Keleta <keletaabraham@gmail.com>
2020-11-18 20:29:55 -0800

    updating with new version

--- custom.js
+++ custom.js
@@ -1,43 +1,40 @@
-var number_Of_todos = 0;
-// strike each todo when completed 
-function strikeContent (element_id) {
-    var element = document.getElementById(element_id);
-    var elementParent = document.getElementById("list-item-" + element_id);
-    var itemContent = element.innerHTML;
-    var newItemContent = itemContent.strike();
-    element.innerHTML = newItemContent;
-    elementParent.style.backgroundColor = "#eceff3";
-    element.style.color = "gray";
-    return false;
-}
-// adding a new todo
-function add_new_todo () {
-    var newNumberOfTodo = number_Of_todos + 1;
-    var todoParent = document.getElementById('list');
-    var inputValue = document.getElementById('new_todo_input').value;
-    var onclickFunctionString = "strikeContent('" + newNumberOfTodo + "')";
-    //create list
-    var new_todo_list = document.createElement('LI');
-    new_todo_list.className = "list-group-item";
-    new_todo_list.id = "list-item-" + (newNumberOfTodo);
-    new_todo_list.setAttribute("onclick", onclickFunctionString);
-    // create a tag 
-    var new_todo_button = document.createElement('A');
-    new_todo_button.className = "item-button";
-    new_todo_button.id = "" + (newNumberOfTodo);
-    // Add input text to a tag
-    var textnode = document.createTextNode(inputValue);
-    // append text to a tag
-    new_todo_button.appendChild(textnode);
-    // append a tag to list
-    new_todo_list.appendChild(new_todo_button);
-    // append list to group
-    todoParent.appendChild(new_todo_list);
-    // empty input 
-    document.getElementById('new_todo_input').value = "";
 
-    //update counter 
-    number_Of_todos++;
-    return false;
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var scrollLocation = document.getElementById("new-todo-group");
+    scrollLocation.scrollTop = scrollLocation.scrollHeight;
+    var number_Of_todos = 0;
+    var link = document.getElementById('add_new_todo_btn');
+    var completed_todo = document.getElementById('list');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        var newNumberOfTodo = number_Of_todos + 1;
+        var todoParent = document.getElementById('list');
+        var inputValue = document.getElementById('new_todo_input').value;
+        //create list
+        var new_todo_list = document.createElement('LI');
+        new_todo_list.className = "list-group-item";
+        new_todo_list.id = "list-item-" + (newNumberOfTodo);
+        new_todo_list.innerHTML = inputValue;
+        // append list to group
+        todoParent.appendChild(new_todo_list);
+        // empty input 
+        document.getElementById('new_todo_input').value = "";
 
+        //update counter 
+        number_Of_todos++;
+        return false;
+    });
+
+    completed_todo.addEventListener('click', function(e) {
+  
+        // find the index of the clicked target from the images array
+        var todos = [].slice.call(completed_todo.querySelectorAll('.list-group-item'));
+        var index = todos.indexOf(e.target) + 1;
+        var elementParent = document.getElementById("list-item-" + index);
+        console.log(index);
+        elementParent.innerHTML = elementParent.innerHTML.strike();
+        elementParent.style.backgroundColor = "#eceff3";
+        elementParent.style.color = "gray";
+        return false;
+    });
+});
